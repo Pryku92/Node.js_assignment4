cc2157fbfd3ac3ca5d5986b2dd39b11963ce481f
Rafal Prykiel <rafalp92@o2.pl>
2019-07-11 21:24:32 +0200

    regular commit

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+
 app.use((req, res, next) => {
     res.status(404).render('page-not-found', {pageTitle: 'Page Not Found'});
 });
