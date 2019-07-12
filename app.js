e19394693e5341bd587c571dc97ea7e6453d81a5
Rafal Prykiel <rafalp92@o2.pl>
2019-07-12 18:54:51 +0200

    regular commit

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-//NODE CORE MODULES IMPORTS
-const path = require('path');
-
-//3RD PARTY PACKAGES IMPORTS
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const userArray = [];
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/', (req, res, next) => {
+    res.render('form', {pageTitle: 'Form Page'});
+});
 
+app.post('/', (req, res, next) => {
+    if(req.body.name !== '') {userArray.push(req.body.name)};
+    res.redirect('/users');
+});
 
-app.use((req, res, next) => {
-    res.status(404).render('page-not-found', {pageTitle: 'Page Not Found'});
+app.get('/users', (req, res, next) => {
+    res.render('list', {pageTitle: 'User list', usrArr: userArray})
 });
 
 app.listen(8000);
